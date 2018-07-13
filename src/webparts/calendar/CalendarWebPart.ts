6953434f17160de2701fd4d00f80b6b801d271b4
Tse Kit Yam <me@kytse.com>
2018-07-13 12:25:26 +0800

    first commit

---
+++ src/webparts/calendar/CalendarWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'CalendarWebPartStrings';
+import Calendar from './components/Calendar';
+import { ICalendarProps } from './components/ICalendarProps';
+
+export interface ICalendarWebPartProps {
+  description: string;
+}
+
+export default class CalendarWebPart extends BaseClientSideWebPart<ICalendarWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<ICalendarProps > = React.createElement(
+      Calendar,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
